f72852e8eb70716dde97efc72df123bcdd4528e1
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-04-19 13:36:16 +0900

    feat: save and load

---
+++ Introduction_to_PyTorch/save_and_load_the_model.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision.models as models
+
+
+# model = models.vgg16(weights='IMAGENET1K_V1')
+# torch.save(model.state_dict(), 'model_weights.pth')
+
+model = models.vgg16()
+model.load_state_dict(torch.load('model_weights.pth'))
+# print(model.eval())
+
+torch.save(model, 'model.pth')
+model = torch.load('model.pth')
